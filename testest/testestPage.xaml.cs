fe46e0e1bc87969303f64d4f1c618a57afbc8dd6
chomado <chomado@gmail.com>
2017-02-14 14:01:07 +0900

    Init

---
+++ testest/testestPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace testest
+{
+    public partial class testestPage : ContentPage
+    {
+        public testestPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
